474e8ada39fd076b99cf4615205a3603b4daf5b9
Ashley Gorringe <ashley@splycd.co.uk>
2022-06-15 19:18:02 +0100

    Development

--- routes/list/new.php
+++ routes/list/new.php
@@ -1,6 +1,17 @@
 <?php
-$response->status = 'success';
-$response->message = 'Test';
-echo json_encode($response);
-exit;
+$list_id = randomStringCaps(30);
+try{
+	$GLOBALS[database]->insert('list',[
+		'list_id'=>$list_id,
+	]);
+	$response->status = 'success';
+	$response->list_id = $list_id;
+	echo json_encode($response);
+	exit;
+}catch(Exception $e){
+	$response->status = 'error';
+	$response->message = $e;
+	echo json_encode($response);
+	exit;
+}
 ?>
