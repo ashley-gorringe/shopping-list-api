121a263e32a03ada07bd01d7d2b3028bb87f2b7e
Ashley Gorringe <ashley@splycd.co.uk>
2022-06-17 23:00:50 +0100

    Added item count limits

--- routes/item/new.php
+++ routes/item/new.php
@@ -16,32 +16,43 @@ if($token_count != 1){
 	$response->message = 'Token is not valid.';
 	echo json_encode($response);
 	exit;
-}else{
-	if(empty($_GET['item_n'])){
-		$response->status = 'error';
-		$response->message = 'Item name has not been declared.';
-		echo json_encode($response);
-		exit;
-	}
+}
 
-	$list_id = $GLOBALS[database]->get('token','list_id',[
-		'token'=>$token,
-	]);
-
-	$GLOBALS[database]->insert('item',[
-		'list_id'=>$list_id,
-		'name'=>$_GET['item_n'],
-		'description'=>$_GET['description'],
-	]);
-	$item_id = $GLOBALS[database]->id();
-
-	$item = $GLOBALS[database]->get('item','*',[
-		'id'=>$item_id
-	]);
-
-	$response->status = 'success';
-	$response->item = $item;
+if(empty($_GET['item_n'])){
+	$response->status = 'error';
+	$response->message = 'Item name has not been declared.';
 	echo json_encode($response);
 	exit;
 }
+
+$list_id = $GLOBALS[database]->get('token','list_id',[
+	'token'=>$token,
+]);
+
+$item_count = $GLOBALS[database]->count('item',[
+	'list_id'=>$list_id
+]);
+
+if($item_count >= 100){
+	$response->status = 'error';
+	$response->message = 'Item limit reached.';
+	echo json_encode($response);
+	exit;
+}
+
+$GLOBALS[database]->insert('item',[
+	'list_id'=>$list_id,
+	'name'=>$_GET['item_n'],
+	'description'=>$_GET['description'],
+]);
+$item_id = $GLOBALS[database]->id();
+
+$item = $GLOBALS[database]->get('item','*',[
+	'id'=>$item_id
+]);
+
+$response->status = 'success';
+$response->item = $item;
+echo json_encode($response);
+exit;
 ?>
